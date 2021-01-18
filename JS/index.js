6c49547f9e5d40a910f59b5b979dac6c0edbf74c
LouPellegrino <louise_pellegrino@hotmail.com>
2021-01-18 13:39:35 +1100

    commited updates

---
+++ JS/index.js
@@ -0,0 +1,256 @@
+/* ***************************
+  JWD JavaScript Assessment
+
+  This code is unfinished. You will need to study it to figure out what it does. Then you will need to use this and
+  your own code, to finish the app. 
+  
+  The tasks you need to do are below.
+
+    TASKS TODO:
+      1. Calculate the score as the total of the number of correct answers
+
+      2. Add an Event listener for the submit button, which will display the score and highlight 
+         the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
+
+      3. Add 2 more questions to the app (each question must have 4 options).
+
+      4. Reload the page when the reset button is clicked (hint: search window.location)
+
+      5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
+*************************** */
+
+// Added widget for the timer - code starts below here
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 60;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+//widget for the timer ends here
+
+//display the quiz starts from here
+
+window.addEventListener('DOMContentLoaded', () => {
+  const start = document.querySelector('#start');
+  start.addEventListener('click', function (e) {
+    document.querySelector('#quizBlock').style.display = 'block';
+    start.style.display = 'none';
+  });
+  const submitButton = document.querySelector('#btnSubmit');
+  submitButton.onclick = function (e) {
+    document.quizBlock.score.innerHTML =`You got a score of ${calculateScore}`;
+  }
+  
+  
+  // quizArray QUESTIONS & ANSWERS
+  // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
+  // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
+  const quizArray = [
+    {
+      q: 'Which is the third planet from the sun?',
+      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
+      a: 1, // array index 1 - so Earth is the correct answer here
+    },
+    {
+      q: 'Which is the largest ocean on Earth?',
+      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      a: 3,
+    },
+    {
+      q: 'What is the capital of Australia?',
+      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+      a: 1,
+    },
+    {
+      q: "What is earth's largest continent?",
+      o: ['Europe', 'Antartica', 'Asia', 'Africa'],
+      a: 2,
+    },
+    {
+      q: "What is the least visisted country in the world?",
+      o: ['Tuvalu', 'Soloman Islands', 'Tonga', 'American Samoa'],
+      a: 0,
+    }
+  ];
+
+  // function to Display the quiz questions and answers from the object
+  const displayQuiz = () => {
+    const quizWrap = document.querySelector('#quizWrap');
+    let quizDisplay = '';
+    quizArray.map((quizItem, index) => {
+      quizDisplay += `<ul class="list-group">
+                   Q - ${quizItem.q}
+                    <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
+                    <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
+                    <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
+                    <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
+                    </ul>
+                    <div>&nbsp;</div>`;
+      quizWrap.innerHTML = quizDisplay;
+    });
+  };
+
+  // Calculate the score
+  const calculateScore = () => {
+    const scoreDisplay = document.querySelector("#score");
+    let score = 0;
+    quizArray.map((quizItem, index) => {
+      for (let i = 0; i < 4; i++) {
+        //highlight the li if it is the correct answer
+        let li = `li_${index}_${i}`;
+        let r = `radio_${index}_${i}`;
+        liElement = document.querySelector('#' + li);
+        radioElement = document.querySelector('#' + r);
+        if (quizItem.a == i) {
+          //change background color of li element here
+          liElement.style.backgroundColor = "green";
+        }
+        if (radioElement.checked) {
+          // code for task 1 goes here
+          if(quizItem.a == i ){
+            score += 20;
+          }
+          }
+        }
+      }
+    );
+         
+          calculateScore();
+          console.log('calculateScore');
+          scoreDisplay();
+  
+      
+          // function showResults
+          
+          //  const submitButton = document.getElementById('btnSubmit');
+          //  submitButton.onclick = function(){
+          //    quizBlock.score.innerHTML =`You got a score of ${calculateScore}`;
+          //  }
+        
+
+        }
+          
+          // getelementbyId.quizBlock.innerHTML = `You have answered ${quizItem[a].score} of ${quizItem.data.length} correctly.`;
+          
+          // call the displayQuiz function
+          displayQuiz();
+                  
+        });
+        
+   
+
+         
+         
+          
+
+    
+   
+
